eca84d35d8dd6894d55f2d65ed3ee0df43520b5f
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-03-29 23:47:01 +0100

    Set up basic HTTP server with GET /jobs route

--- jobpilot/handler/job_handler.go
+++ jobpilot/handler/job_handler.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"encoding/json"
 	"jobpilot/model"
+	"net/http"
 	"sync"
 )
 
@@ -10,3 +12,16 @@ var (
 	nextID = 1
 	mu     sync.Mutex
 )
+
+func GetJobs(w http.ResponseWriter, r *http.Request) {
+	enableCORS(&w)
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(jobs)
+	if err != nil {
+		return
+	}
+}
+
+func enableCORS(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
--- jobpilot/main.go
+++ jobpilot/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"jobpilot/handler"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, JobPilot!")
+	http.HandleFunc("/jobs", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			handler.GetJobs(w, r)
+		default:
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		}
+	})
+	fmt.Println("🚀 JobPilot backend running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
